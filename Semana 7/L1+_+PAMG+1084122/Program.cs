e7ff6f7d808fbb950d723704be80c4da36d9d4fa
paolaamg <111013050+paolaamg@users.noreply.github.com>
2022-09-21 20:12:32 -0600

    Semana 7

---
+++ Semana 7/L1+_+PAMG+1084122/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L1___PAMG_1084122
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { Console.WriteLine("Hola Mundo soy Paola Morales");
+            Console.ReadKey();
+
+            /*Write se utiliza para para imprimir datos sin imprimir la nueva línea.*/
+            Console.Write("Hola Mundo");
+            Console.Write("Soy Paola");
+            Console.ReadKey();
+
+            Console.WriteLine("Ingrese su nombre:");
+            String Nombre = Console.ReadLine();
+
+            Console.WriteLine("Hola Mundo");
+            Console.WriteLine("Soy Paola");
+
+            /*WriteLine se utiliza para imprimir datos con la impresión de la nueva línea.*/
+            Console.Write("Hola Mundo");
+            Console.Write("Soy Paola");
+            Console.ReadKey();
+        }
+
+
+    }
+        
+}
